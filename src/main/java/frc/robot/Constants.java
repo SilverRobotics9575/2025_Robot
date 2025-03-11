d682f926d0b162cf64b42da1da9f2aaff6d9ef1e
FatumaT <regretmakingthis@gmail.com>
2025-03-11 18:15:35 -0400

    No level 4. I BAN IT

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -43,7 +43,6 @@ public final class Constants {
         public static final int     kLevel1                  = 0;
         public static final int     kLevel2                  = 0;
         public static final int     kLevel3                  = 100;
-        public static final int     kLevel4                  = 150;
 
         public static final int MAXHEIGHT_ID = 1;
         public static final int MINHEIGHT_ID = 0;
