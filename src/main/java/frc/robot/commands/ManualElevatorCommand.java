6026edb24a41674291b2376e8b8c59f0df685f63
FatumaT <regretmakingthis@gmail.com>
2025-03-14 06:46:29 -0400

    Change manual elevator to be controlled by trigger not button

--- src/main/java/frc/robot/commands/ManualElevatorCommand.java
+++ src/main/java/frc/robot/commands/ManualElevatorCommand.java
@@ -31,11 +31,13 @@ public class ManualElevatorCommand extends LoggingCommand{
   @Override
   public void execute() {
          // Manual control buttons
-        if (oi.elevatorUp()) {
-            elevatorSubsystem.setElevatorSpeed(ElevatorConstants.CAN_ELEVATOR_MOTOR_SPEED, false, oi.overrideLimit());
+        if (oi.elevatorUp() > 0) {
+            elevatorSubsystem.setElevatorSpeed(oi.elevatorUp() * ElevatorConstants.CAN_ELEVATOR_MOTOR_SPEED, 
+              false, oi.overrideLimit());
         }
-        else if (oi.elevatorDown()) {
-            elevatorSubsystem.setElevatorSpeed(-ElevatorConstants.CAN_ELEVATOR_MOTOR_SPEED, true, oi.overrideLimit());
+        else if (oi.elevatorDown() > 0) {
+            elevatorSubsystem.setElevatorSpeed(- oi.elevatorUp() * ElevatorConstants.CAN_ELEVATOR_MOTOR_SPEED, 
+            true, oi.overrideLimit());
         }
   }
 
